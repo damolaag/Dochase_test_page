839c1e3005af9a410480a6b96c51f2c8443eb945
damolaag <damolaagbonhin@gmail.com>
2023-09-07 13:52:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,125 @@
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+	<meta content="This is an automatically generated default server page successfully deployed by the web hosting provider. Client has not uploaded site." name="Description">
+	<meta content="NOODP" name="robots">
+	<title>New Website</title>
+	<link href="https://gigalayer.com/default/styles.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript">
+	var domain =  document.location.host; 
+	function cpLogin() { window.location.href = 'http://' + domain + '/cpanel'; }
+	function emailLogin() { window.location.href = 'http://' + domain + '/webmail'; }
+	function goHelp() { window.location.href = 'https://core.gigalayer.com/knowledgebase.php'; }
+	function goBilling() { window.location.href = 'https://core.gigalayer.com/'; }
+	function goTransfer() { window.location.href = 'https://gigalayer.com/site-transfer'; }
+	</script>
+	</head>
+	<body>
+	<div id="header" class="slimheader">
+
+	</div>
+	<div id="contentholder">
+	<div id="content">
+	<div id="subhead">
+	<em class="hostname">
+	Poweredby: <a href="http://gigalayer.com">gigalayer.com</a>
+	</em>
+	<h1>	<script type="text/javascript">
+	document.write(domain);
+	</script></h1>
+	<hr>
+	<p>
+	Welcome to your new website... If you are the owner of this site; you need to upload your website to replace this page.
+	</p>
+	</div>
+	<div id="checklist">
+		<ul>
+		<li class="left">
+		<a onclick="goBilling();"  href="#knowledgebase">
+		<img alt="Client Area" src="https://gigalayer.com/default/user_acc.png">
+		<strong>Billing Account</strong>
+		</a>
+		</li>
+		<li class="middle">
+		<a onclick="cpLogin();" href="#cpanel">
+		<img alt="cPanel Account" src="https://gigalayer.com/default/cpanel_acc.png">
+		<strong>cPanel Login</strong>
+		</a>
+		</li>
+		<li class="middle">
+		<a onclick="goHelp();"  href="#knowledgebase">
+		<img alt="Knoledgebase" src="https://gigalayer.com/default/cp_wiki.png">
+		<strong>Self Support</strong>
+		</a>
+		</li>
+		<li class="middle">
+		<a onclick="emailLogin();" href="#webmail">
+		<img alt="WebMail" src="https://gigalayer.com/default/email_acc.png">
+		<strong>Email Login</strong>
+		</a>
+		</li>
+		
+		
+		<li class="middle">
+		<a onclick="goTransfer();" href="#transfer">
+		<img alt="Knoledgebase" src="https://gigalayer.com/default/transfer_site.png">
+		<strong>Import Website</strong>
+		</a>
+		</li>
+		</ul>
+		<div class="clearfix">&nbsp;</div>
+	</div>
+	<div id="kblinks">
+	<dl>
+	<dt>
+	<a href="http://gigalayer.com">Go to main site »</a>
+	<span><h2>Other GigaLayer Products:</h2></span>
+	</dt>
+	<dd class="plesk_guide">
+	<div>
+	<span>
+	<a href="https://gigalayer.com/domain-names">Domain Names</a>
+	</span>
+	</div>
+	</dd>
+	<dd class="add_domain">
+	<div>
+	<span>
+	<a href="https://gigalayer.com/web-hosting">Shared Hosting</a>
+	</span>
+	</div>
+	</dd>
+	<dd class="add_email">
+	<div>
+	<span>
+	<a href="http://gigalayer.com/vps-hosting">VPS Hosting</a>
+	</span>
+	</div>
+	</dd>
+	<dd class="data_backup enddd">
+	<div>
+	<span>
+	<a href="http://gigalayer.com/ssl-certificates">SSL Certificates</a>
+	</span>
+	</div>
+	</dd>
+	</dl>
+	<div class="clearfix">&nbsp;</div>
+	</div>
+	</div>
+	</div>
+	<div id="footer">
+	<div id="footerdiv">
+	<div class="supportfooter">
+	<div style="float:right; margin-top:30px;">Sales: <strong>0802-5594-018</strong></div>
+
+	<p class="copyright fl-left">
+	&copy; 2007 - 2017 GigaLayer.com
+	</p>
+	</div>
+	<div class="clearfix">&nbsp;</div>
+	</div>
+	</div>
+	</body>
+	</html>
